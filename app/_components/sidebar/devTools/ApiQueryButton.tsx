b577db4f76c1a1c18065031aca48df46d160f6f1
Lewis Steentjes <lpsteentjes@gmail.com>
2024-04-26 21:23:27 +1200

    Disable our test button

--- app/_components/sidebar/devTools/ApiQueryButton.tsx
+++ app/_components/sidebar/devTools/ApiQueryButton.tsx
@@ -8,17 +8,10 @@ interface Props {
 }
 
 export default function ApiQueryButton(props: Props) {
-  const handleClick = async () => {
-    const result = await getTradeMe(bounds);
-    console.log(result);
-  };
-
   return (
     <>
       <br />
-      <button onClick={handleClick} className="btn">
-        Test Server
-      </button>
+      <button className="btn">Test Server</button>
       <br />
       <br />
       <span>Filter Options:</span>
